03f8d93fa8b1c8cf7dfdf38bd96465a71053bd78
Daniel Chia <daniel_1118@hotmail.com>
2016-03-16 18:26:14 -0400

    init commit, migrated files from flask-practice/kendama-list

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# kendama-collection
-Flask app to show pictures and descriptions of the kendamas I have got
+# Kendama List Application
+
+This application will display kendamas in a Instagram like grid view.
+
+Primarily starting this app to work with WTForms
+
+## User stories
+
+* [ ] The main page displays a 3 column grid that displays pictures of kendamas
+* [ ] Under the image, there is a header with the name and a table with further description, brand, link to buy
+* [ ] There is a form, that allows me to add a name and link to the kendama picture
+* [ ] There is a tab with instagram tab with the #kendama filter to see kendama posts
+
+## TODOS
+
+* Set up dummy html page to play with components
+* Display description of your personal kendama in a neat table
+* Create custom CSS to set the images to a certain height (350px) and apply center-block class
+* Replace images with shots of your own
+
+## Notes
+
+* http://statusq.org/archives/2014/10/25/6161/ to set up Bower with Flask
+* https://github.com/BlackrockDigital/startbootstrap-3-col-portfolio 3 column instagram looking page
+
---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+# =============================== MODELS ===============================
+# taken from the basic app, so db is created in this same dir
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI 
+
+db = SQLAlchemy(app)
+
+class Kendama(db.Model):
+    # link has to be validated as a url in the form
+    # Name | Brand | Link | Description
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    brand = db.Column(db.String(50))
+    link = db.Column(db.String(100))
+    description = db.Column(db.Text())
+
+    def __init__(self, name, brand, link, description):
+        self.name = name
+        self.brand = brand
+        self.description = description
+        self.link = link
+
+    def __repr__(self):
+        return '<Kendama %r>' % self.name
+
+# =============================== VIEWS ===============================
+
+@app.route("/")
+def hello():
+    return render_template("base.html") 
+
+
+@app.route("/kendamas")
+def kendama_list():
+    return "Hey, you currently have no kendamas"
+
+
+@app.route("/testing")
+def testing_components():
+
+    return render_template("dummy.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
