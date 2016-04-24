d319fe9592345d639f7b64ec47372ba340d5c81c
Daniel Chia <daniel_1118@hotmail.com>
2016-04-24 19:36:08 -0400

    kendama blocks now fade away on client side

--- static/js/main.js
+++ static/js/main.js
@@ -5,8 +5,33 @@
 
 $("#gone").on("click", function(){
 
+	var parentelement = $(this).parent();
+
 	//use this to refer to current element
-	console.log($(this).parent());
+	console.log("this is the parent ", parentelement);
+
+	//it's not logging the same thing but seems to target the same element
+	parentelement.fadeOut("slow", function(){
+		//should be the same element being removed
+		console.log("this is hopefully still the parent ", $(this) );
+
+		$(this).remove();	
+	});
 
 });
+
+
+//from the delete button, two levels up to target the kendama block
+$(".delete-ken").on("click", function(){
+
+	//first parent is the header, then the div
+	var parentEle = $(this).parent().parent();
+
+	parentEle.fadeOut("slow", function(){
+		$(this).remove();
+	});
+
+});
+
+
 
